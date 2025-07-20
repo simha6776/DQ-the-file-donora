50a991f1cebf6f17cff0a6ac6bb23edb9cee9260
NARASIMHAMURTHY H T <118955214+simha6776@users.noreply.github.com>
2025-07-20 21:09:28 +0530

    Update info.py

--- info.py
+++ info.py
@@ -43,7 +43,7 @@ NO_RESULTS_MSG = is_enabled((environ.get("NO_RESULTS_MSG", 'False')), False)
 
 # MongoDB information
 SECONDDB_URI = environ.get('SECONDDB_URI', None)
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://narasimhamurthyht9:aIxFw22N5brWKpIL@cluster0.l8io8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://narasimhamurthyht9:Narasimha@cluster0.l8io8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
